55bf039e1f8d913759773a2a850b74c7493464f7
casalecchi <felipevilela191@gmail.com>
2024-08-27 21:23:40 -0300

    install MUI

---
+++ src/Field.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+import { FC } from "react"
+
+const Field: FC = () => {
+    return (
+        <Box
+            height={"10rem"}
+            justifyContent={"center"}
+            sx={{ backgroundColor: "green" }}
+            width={"1rem"}
+        ></Box>
+    )
+}
+
+export default Field
