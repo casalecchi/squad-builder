0ef1301610f423d671bed2a62ad4fdd4dd02d247
casalecchi <felipevilela191@gmail.com>
2024-08-28 15:55:48 -0300

    setup for i18next

---
+++ src/lib/i18n.ts
@@ -0,0 +1,12 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from '../lang/en.json'
+import pt from '../lang/pt.json'
+
+i18n.use(initReactI18next).init({
+    resources: {
+        en: en,
+        pt: pt,
+    },
+    lng: 'en',
+})
