9c0f52b8d3e58e7777a7b2808af73cbeecffb4e8
casalecchi <felipevilela191@gmail.com>
2024-08-29 18:20:42 -0300

    refactor: create Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import Header from './components/header'
+import Page from './components/page'
 import theme from './configurations/theme'
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline enableColorScheme>
-                <Header />
+                <Page />
             </CssBaseline>
         </ThemeProvider>
     )
---
+++ src/components/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import { Stack } from '@mui/material'
+import Header from './header'
+
+const Page: FC = () => {
+    return (
+        <Stack>
+            <Header />
+        </Stack>
+    )
+}
+
+export default Page
