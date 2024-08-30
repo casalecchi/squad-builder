44eb7d7a37727a08e44f23e0f0b10cbeb0611de0
casalecchi <felipevilela191@gmail.com>
2024-08-30 11:57:00 -0300

    change import for `i18n.ts`

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './lib/i18n'
+import './configurations/i18n.ts'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
