b9cf351afef129b11fafd3076897cd3fa11e4b1f
casalecchi <felipevilela191@gmail.com>
2024-09-02 16:47:17 -0300

    implement state of formation

--- src/components/builder.tsx
+++ src/components/builder.tsx
@@ -1,23 +1,25 @@
 import { Box, Stack, Typography } from '@mui/material'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import pitch from '../assets/campinho.svg'
 import { useTranslation } from 'react-i18next'
 import FormationSelector from './formationSelector'
-import { fourFiveOne } from '../utils/formations'
+import { fourThreeThree } from '../utils/formations'
 import Team from './team'
+import { Formation } from '../models'
 
 const Builder: FC = () => {
     const { t } = useTranslation()
+    const [formation, setFormation] = useState<Formation>(fourThreeThree)
 
     return (
         <Stack alignItems={'center'}>
             <Typography mb={2} variant="h3">
                 {t('builder.lineup').toUpperCase()}
             </Typography>
-            <FormationSelector mb={2} />
+            <FormationSelector formationSelected={formation} mb={2} setFormation={setFormation} />
             <Box height={'85vh'} position={'relative'}>
                 <img alt={t('images.pitch')} height={'100%'} src={pitch} />
-                <Team formation={fourFiveOne} />
+                <Team formation={formation.playersPositions} />
             </Box>
         </Stack>
     )
