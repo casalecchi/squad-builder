bd18346319bcc1cbaad18b60cf1561af99e5e09e
casalecchi <felipevilela191@gmail.com>
2024-09-02 16:01:31 -0300

    add ContentContainer on page and removing it from builder

--- src/components/builder.tsx
+++ src/components/builder.tsx
@@ -10,22 +10,15 @@ const Builder: FC = () => {
     const { t } = useTranslation()
 
     return (
-        <Stack alignItems={'center'} p={{ xs: 0, md: 1 }}>
-            <Stack
-                direction={{ xs: 'column', md: 'row' }}
-                sx={{ padding: 1, maxWidth: '1280px', width: '100%' }}
-            >
-                <Stack alignItems={'center'}>
-                    <Typography mb={2} variant="h3">
-                        {t('builder.lineup')}
-                    </Typography>
-                    <FormationSelector mb={2} />
-                    <Box height={'85vh'} position={'relative'}>
-                        <img alt={t('images.pitch')} height={'100%'} src={pitch} />
-                        <Team formation={fourFiveOne} />
-                    </Box>
-                </Stack>
-            </Stack>
+        <Stack alignItems={'center'}>
+            <Typography mb={2} variant="h3">
+                {t('builder.lineup')}
+            </Typography>
+            <FormationSelector mb={2} />
+            <Box height={'85vh'} position={'relative'}>
+                <img alt={t('images.pitch')} height={'100%'} src={pitch} />
+                <Team formation={fourFiveOne} />
+            </Box>
         </Stack>
     )
 }
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -3,12 +3,15 @@ import { Stack } from '@mui/material'
 import Header from './header'
 import Footer from './footer'
 import Builder from './builder'
+import ContentContainer from './ui/contentContainer'
 
 const Page: FC = () => {
     return (
         <Stack display={'flex'}>
             <Header />
-            <Builder />
+            <ContentContainer>
+                <Builder />
+            </ContentContainer>
             <Footer />
         </Stack>
     )
