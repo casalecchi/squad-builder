d076e31fc7d7c8a7d23bce90ad8e90a9687e49a7
casalecchi <felipevilela191@gmail.com>
2024-09-04 13:25:29 -0300

    fetch Cartola market data done - still trying to fetch cartola player images

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,16 +1,57 @@
-import { FC } from 'react'
-import { Stack } from '@mui/material'
+import { FC, useEffect, useState } from 'react'
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Stack } from '@mui/material'
 import Header from './header'
 import Footer from './footer'
 import Builder from './builder'
 import ContentContainer from './ui/contentContainer'
+import useFetchCartola from '../hooks/useFetchCartola'
 
 const Page: FC = () => {
+    const { players, fetchData } = useFetchCartola()
+
+    const [images, setImages] = useState<{ [key: number]: string }>({})
+
+    // useEffect(() => {
+    //     const fetchImages = async () => {
+    //         const imagesMap: { [key: number]: string } = {}
+
+    //         for (const player of players) {
+    //             try {
+    //                 const response = await fetch(player.photo, { mode: 'no-cors' })
+    //                 const blob = await response.blob()
+    //                 const imageUrl = URL.createObjectURL(blob)
+    //                 imagesMap[player.id] = imageUrl
+    //             } catch (error) {
+    //                 console.error('Erro ao carregar imagem:', error)
+    //             }
+    //         }
+
+    //         console.log(imagesMap, 'AQUI')
+    //         setImages(imagesMap)
+    //     }
+
+    //     fetchImages().then()
+    // }, [players])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <Stack display={'flex'}>
             <Header />
             <ContentContainer>
                 <Builder />
+                <List>
+                    {players.slice(0, 1).map((player, index) => (
+                        <ListItem key={index}>
+                            <ListItemAvatar>
+                                <Avatar alt={player.name} src={images[player.id]} />
+                            </ListItemAvatar>
+                            <ListItemText>{player.name}</ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
             </ContentContainer>
             <Footer />
         </Stack>
---
+++ src/hooks/useFetchCartola.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { CartolaResponse, Player } from '../models'
+
+const useFetchCartola = () => {
+    const URL = 'https://api.cartola.globo.com/atletas/mercado'
+    const [players, setPlayers] = useState<Player[]>([])
+
+    const fetchData = async () => {
+        console.log('CARREGANDO...')
+        try {
+            await fetch(URL)
+                .then((response) => response.json())
+                .then((data: CartolaResponse) => {
+                    setPlayers(
+                        data.atletas.map((atleta) => ({
+                            id: atleta.atleta_id,
+                            teamId: atleta.clube_id,
+                            positionId: atleta.posicao_id,
+                            statusId: atleta.status_id,
+                            name: atleta.apelido,
+                            price: atleta.preco_num,
+                            photo: atleta.foto?.replace('FORMATO', '220x220') ?? '',
+                        }))
+                    )
+                })
+                .catch((error) => console.error(error))
+        } catch {
+            console.log('Error fetching Cartola players')
+        }
+    }
+
+    return { fetchData, players }
+}
+
+export default useFetchCartola
