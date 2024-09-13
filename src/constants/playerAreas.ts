295992ca4fe13c0bab9a7ab2b6ddd5c9118df4d2
casalecchi <felipevilela191@gmail.com>
2024-09-13 10:43:54 -0300

    adjust player areas

--- src/constants/playerAreas.ts
+++ src/constants/playerAreas.ts
@@ -4,15 +4,15 @@ import { PlayerArea } from '../models'
 export const goalkeeperPosition: PlayerArea = { code: 'gk', bottom: '2%', left: '50%' }
 
 // --------------------------------- DEFENDERS ----------------------------------------
-export const threeRightCenterback: PlayerArea = { code: 'cb', bottom: '20%', left: '75%' }
-export const threeCenterCenterback: PlayerArea = { code: 'cb', bottom: '20%', left: '50%' }
-export const threeLeftCenterback: PlayerArea = { code: 'cb', bottom: '20%', left: '25%' }
+export const threeRightCenterback: PlayerArea = { code: 'cb', bottom: '22%', left: '75%' }
+export const threeCenterCenterback: PlayerArea = { code: 'cb', bottom: '22%', left: '50%' }
+export const threeLeftCenterback: PlayerArea = { code: 'cb', bottom: '22%', left: '25%' }
 export const threeDefenders = [threeLeftCenterback, threeCenterCenterback, threeRightCenterback]
 
-export const fourRightWingBack: PlayerArea = { code: 'wb', bottom: '30%', left: '80%' }
-export const fourRightCenterback: PlayerArea = { code: 'cb', bottom: '22%', left: '60%' }
-export const fourLeftCenterback: PlayerArea = { code: 'cb', bottom: '22%', left: '40%' }
-export const fourLeftWingBack: PlayerArea = { code: 'wb', bottom: '30%', left: '20%' }
+export const fourRightWingBack: PlayerArea = { code: 'wb', bottom: '27%', left: '80%' }
+export const fourRightCenterback: PlayerArea = { code: 'cb', bottom: '21%', left: '60%' }
+export const fourLeftCenterback: PlayerArea = { code: 'cb', bottom: '21%', left: '40%' }
+export const fourLeftWingBack: PlayerArea = { code: 'wb', bottom: '27%', left: '20%' }
 export const fourDefenders = [
     fourLeftWingBack,
     fourRightWingBack,
@@ -20,11 +20,11 @@ export const fourDefenders = [
     fourRightCenterback,
 ]
 
-export const fiveRightWingBack: PlayerArea = { code: 'wb', bottom: '33%', left: '80%' }
-export const fiveRightCenterback: PlayerArea = { code: 'cb', bottom: '25%', left: '65%' }
-export const fiveCenterCenterback: PlayerArea = { code: 'cb', bottom: '25%', left: '50%' }
-export const fiveLeftCenterback: PlayerArea = { code: 'cb', bottom: '25%', left: '35%' }
-export const fiveLeftWingBack: PlayerArea = { code: 'wb', bottom: '33%', left: '20%' }
+export const fiveRightWingBack: PlayerArea = { code: 'wb', bottom: '34%', left: '80%' }
+export const fiveRightCenterback: PlayerArea = { code: 'cb', bottom: '21%', left: '68%' }
+export const fiveCenterCenterback: PlayerArea = { code: 'cb', bottom: '21%', left: '50%' }
+export const fiveLeftCenterback: PlayerArea = { code: 'cb', bottom: '21%', left: '32%' }
+export const fiveLeftWingBack: PlayerArea = { code: 'wb', bottom: '34%', left: '20%' }
 export const fiveDefenders = [
     fiveLeftWingBack,
     fiveRightWingBack,
@@ -34,15 +34,15 @@ export const fiveDefenders = [
 ]
 
 // --------------------------------- MIDFIELDERS ---------------------------------------
-export const threeRightMidfielder: PlayerArea = { code: 'mid', bottom: '47%', left: '63%' }
-export const threeCenterMidfielder: PlayerArea = { code: 'mid', bottom: '57%', left: '50%' }
-export const threeLeftMidfielder: PlayerArea = { code: 'mid', bottom: '47%', left: '37%' }
+export const threeRightMidfielder: PlayerArea = { code: 'mid', bottom: '51%', left: '73%' }
+export const threeCenterMidfielder: PlayerArea = { code: 'mid', bottom: '49%', left: '50%' }
+export const threeLeftMidfielder: PlayerArea = { code: 'mid', bottom: '51%', left: '27%' }
 export const threeMidfielders = [threeLeftMidfielder, threeCenterMidfielder, threeRightMidfielder]
 
-export const fourRightMidfielder: PlayerArea = { code: 'mid', bottom: '57%', left: '75%' }
-export const fourRightCenterMidfielder: PlayerArea = { code: 'mid', bottom: '48%', left: '60%' }
-export const fourLeftCenterMidfielder: PlayerArea = { code: 'mid', bottom: '48%', left: '40%' }
-export const fourLeftMidfielder: PlayerArea = { code: 'mid', bottom: '57%', left: '25%' }
+export const fourRightMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '75%' }
+export const fourRightCenterMidfielder: PlayerArea = { code: 'mid', bottom: '45%', left: '60%' }
+export const fourLeftCenterMidfielder: PlayerArea = { code: 'mid', bottom: '45%', left: '40%' }
+export const fourLeftMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '25%' }
 export const fourMidfielders = [
     fourLeftMidfielder,
     fourLeftCenterMidfielder,
@@ -50,27 +50,27 @@ export const fourMidfielders = [
     fourRightMidfielder,
 ]
 
-export const fiveRightMidfielder: PlayerArea = { code: 'mid', bottom: '60%', left: '80%' }
-export const fiveRightCenterMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '65%' }
-export const fiveLeftCenterMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '35%' }
-export const fiveLeftMidfielder: PlayerArea = { code: 'mid', bottom: '60%', left: '20%' }
-export const fiveCenterMidfielder: PlayerArea = { code: 'mid', bottom: '42%', left: '50%' }
+export const fiveRightMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '75%' }
+export const fiveRightCenterMidfielder: PlayerArea = { code: 'mid', bottom: '45%', left: '60%' }
+export const fiveLeftCenterMidfielder: PlayerArea = { code: 'mid', bottom: '45%', left: '40%' }
+export const fiveLeftMidfielder: PlayerArea = { code: 'mid', bottom: '50%', left: '25%' }
+export const fiveCenterMidfielder: PlayerArea = { code: 'mid', bottom: '58%', left: '50%' }
 export const fiveMidfielders = [
     fiveLeftMidfielder,
     fiveLeftCenterMidfielder,
-    fiveCenterMidfielder,
     fiveRightCenterMidfielder,
     fiveRightMidfielder,
+    fiveCenterMidfielder,
 ]
 
 // --------------------------------- STRIKERS -----------------------------------------
-export const oneStriker: PlayerArea = { code: 'st', bottom: '80%', left: '50%' }
+export const oneStriker: PlayerArea = { code: 'st', bottom: '73%', left: '50%' }
 
-export const twoRightStriker: PlayerArea = { code: 'st', bottom: '75%', left: '60%' }
-export const twoLeftStriker: PlayerArea = { code: 'st', bottom: '75%', left: '40%' }
+export const twoRightStriker: PlayerArea = { code: 'st', bottom: '73%', left: '60%' }
+export const twoLeftStriker: PlayerArea = { code: 'st', bottom: '73%', left: '40%' }
 export const twoStrikers = [twoLeftStriker, twoRightStriker]
 
-export const threeRightStriker: PlayerArea = { code: 'st', bottom: '75%', left: '75%' }
-export const threeCenterStriker: PlayerArea = { code: 'st', bottom: '80%', left: '50%' }
-export const threeLeftStriker: PlayerArea = { code: 'st', bottom: '75%', left: '25%' }
+export const threeRightStriker: PlayerArea = { code: 'st', bottom: '70%', left: '75%' }
+export const threeCenterStriker: PlayerArea = { code: 'st', bottom: '73%', left: '50%' }
+export const threeLeftStriker: PlayerArea = { code: 'st', bottom: '70%', left: '25%' }
 export const threeStrikers = [threeLeftStriker, threeCenterStriker, threeRightStriker]
