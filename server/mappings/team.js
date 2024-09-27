a041b955f4bb6eaae4b7cc513ad76a145de7ed61
casalecchi <felipevilela191@gmail.com>
2024-09-26 22:55:27 -0300

    create manual Map for cartola Id to sofascore Id

---
+++ server/mappings/team.js
@@ -0,0 +1,24 @@
+const teamMap = {
+    262: 5981, // Flamengo
+    263: 1958, // Botafogo
+    264: 1957, // Corinthians
+    265: 1955, // Bahia
+    266: 1961, // Fluminense
+    267: 1974, // Vasco
+    275: 1963, // Palmeiras
+    276: 1981, // São Paulo
+    280: 1999, // RB Bragantino
+    282: 1977, // Atletico-MG
+    283: 1954, // Cruzeiro
+    284: 5926, // Grêmio
+    285: 1966, // Internacional
+    286: 1980, // Juventude
+    287: 1962, // Vitoria
+    288: 1984, // Criciuma
+    293: 1967, // Athletico-PR
+    356: 2020, // Fortaleza
+    373: 7314, // Atetico-GO
+    1371: 49202, // Cuiaba
+}
+
+export default teamMap
