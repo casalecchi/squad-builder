db60fa4f81be9816be1e82784802f58d0811f2e3
casalecchi <felipevilela191@gmail.com>
2024-09-28 14:24:51 -0300

    remove test code

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,18 +1,12 @@
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 import { Stack, useMediaQuery } from '@mui/material'
 import Header from './header'
 import Footer from './footer'
 import Builder from './builder'
 import ContentContainer from './ui/contentContainer'
-import useFetchSofascore from '../hooks/useFetchSofascore'
 
 const Page: FC = () => {
     const mobile = useMediaQuery('(max-width:600px)', { noSsr: true })
-    const { fetchData } = useFetchSofascore()
-
-    useEffect(() => {
-        fetchData()
-    }, [])
 
     return (
         <Stack display={'flex'}>
