4c8e01164e2c2ade41b4e1a749daa06e33932e66
casalecchi <felipevilela191@gmail.com>
2024-10-09 20:05:58 -0300

    create status indicator for market

--- src/components/ui/marketDialog.tsx
+++ src/components/ui/marketDialog.tsx
@@ -3,6 +3,8 @@ import { Dialog, DialogTitle, IconButton, Stack, Typography } from '@mui/materia
 import { FC, PropsWithChildren } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDataContext } from '../../contexts/DataContext'
+import { TranslationKey } from '../../@types/i18n'
+import StatusIndicator from './statusIndicator'
 
 const MarketDialog: FC<PropsWithChildren> = ({ children }) => {
     const { t } = useTranslation()
@@ -12,10 +14,21 @@ const MarketDialog: FC<PropsWithChildren> = ({ children }) => {
     return (
         <Dialog fullWidth maxWidth={'lg'} onClose={closeMarket} open={isMarketOpen}>
             <Stack alignItems={'center'} direction={'row'}>
-                <DialogTitle>{t('market.title')}</DialogTitle>
-                <Typography>{`Mercado atualmente está ${marketInfo?.status}`}</Typography>
-                <Typography>{`Rodada ${marketInfo?.round}`}</Typography>
-                <IconButton onClick={closeMarket} sx={{ position: 'absolute', right: 10, top: 8 }}>
+                <DialogTitle>{t('market.title').toUpperCase()}</DialogTitle>
+                <Stack
+                    alignItems={'center'}
+                    sx={{ position: 'absolute', left: '50%', transform: 'translate(-50%, 0%)' }}
+                >
+                    <Stack alignItems={'center'} direction={'row'} spacing={1}>
+                        <Typography>
+                            {t(
+                                `market.status.${marketInfo?.status}` as TranslationKey
+                            ).toUpperCase()}
+                        </Typography>
+                        <StatusIndicator size={'2.5rem'} status={marketInfo?.status ?? 'closed'} />
+                    </Stack>
+                </Stack>
+                <IconButton onClick={closeMarket} sx={{ position: 'absolute', right: 10 }}>
                     <Close />
                 </IconButton>
             </Stack>
