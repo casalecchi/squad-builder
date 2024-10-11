f768212071862a1d1a8b78e54d18c0f58ebc429b
casalecchi <felipevilela191@gmail.com>
2024-10-11 11:11:06 -0300

    new builder screen - start the adjustments

--- src/components/builder/builder.tsx
+++ src/components/builder/builder.tsx
@@ -1,45 +1,62 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Paper, Grid2, Stack, Typography } from '@mui/material'
 import { FC } from 'react'
 import pitch from '../../assets/campinho.svg'
-import { useTranslation } from 'react-i18next'
 import FormationSelector from './formationSelector'
 import Team from './team/team'
 import { useDataContext } from '../../contexts/DataContext'
 import FormationAdjustment from './formationAdjustment'
-import { formatNumber } from '../../utils'
 import Market from './market/market'
-import { CustomCard } from '../ui/customCard'
 
 const Builder: FC = () => {
-    const { t } = useTranslation()
     const { teamStateManager } = useDataContext()
-    const { teamValue, formation, changeFormation } = teamStateManager
+    const { formation, changeFormation } = teamStateManager
 
     return (
-        <CustomCard alignItems={'center'} fixedSize={'100vh'}>
-            <Typography mb={2} variant="h3">
-                {`${t('builder.teamValue').toUpperCase()}: ${t('common.money')}${formatNumber(teamValue)}`}
-            </Typography>
-            <FormationSelector
-                changeFormation={changeFormation}
-                formationSelected={formation}
-                mb={2}
-            />
-            <Box position={'relative'}>
-                <img
-                    alt={t('images.pitch')}
-                    src={pitch}
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'contain',
-                    }}
-                />
-                <Team />
-            </Box>
-            <Market />
-            <FormationAdjustment />
-        </CustomCard>
+        <Grid2 container height={'100vh'} p={2} spacing={2}>
+            <Grid2 height={'100%'} size={5}>
+                <Paper
+                    sx={{ justifyContent: 'center', display: 'flex', flex: 1, height: '100%' }}
+                    variant={'outlined'}
+                >
+                    <Stack alignItems={'center'} height={'100%'} py={2} spacing={2} width={'90%'}>
+                        <FormationSelector
+                            changeFormation={changeFormation}
+                            formationSelected={formation}
+                        />
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                width: '100%',
+                                height: '100%',
+                                maxWidth: 800,
+                                maxHeight: 800,
+                                backgroundImage: `url(${pitch})`,
+                                backgroundSize: 'contain',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                            <Team />
+                        </Box>
+                        <Market />
+                        <FormationAdjustment />
+                    </Stack>
+                </Paper>
+            </Grid2>
+            <Grid2 size={7}>
+                <Stack height={'100%'} spacing={2}>
+                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
+                        <Typography>{'90% de passes certos'}</Typography>
+                    </Paper>
+                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
+                        <Typography>{'10 desarmes por jogo'}</Typography>
+                    </Paper>
+                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
+                        <Typography>{'5 gols por jogo'}</Typography>
+                    </Paper>
+                </Stack>
+            </Grid2>
+        </Grid2>
     )
 }
 
