212e4c1b66d82ebed148ee0b44a3e9d31020bb53
casalecchi <felipevilela191@gmail.com>
2024-10-12 00:19:42 -0300

    organize new app structure

--- src/components/builder/builder.tsx
+++ src/components/builder/builder.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Grid2, Stack, Typography } from '@mui/material'
+import { Box, Stack, useMediaQuery } from '@mui/material'
 import { FC } from 'react'
 import pitch from '../../assets/campinho.svg'
 import FormationSelector from './formationSelector'
@@ -6,57 +6,94 @@ import Team from './team/team'
 import { useDataContext } from '../../contexts/DataContext'
 import FormationAdjustment from './formationAdjustment'
 import Market from './market/market'
+import { StatCard } from './stats/statCard'
+import { CustomPaper } from '../ui/customPaper'
+
+const BoxTeam: FC = () => {
+    return (
+        <Box
+            sx={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                maxWidth: 800,
+                maxHeight: 800,
+                backgroundImage: `url(${pitch})`,
+                backgroundSize: 'contain',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
+            <Team />
+        </Box>
+    )
+}
 
 const Builder: FC = () => {
+    const mobile = useMediaQuery('(max-width:600px)', { noSsr: true })
     const { teamStateManager } = useDataContext()
     const { formation, changeFormation } = teamStateManager
 
     return (
-        <Grid2 container height={'100vh'} p={2} spacing={2}>
-            <Grid2 height={'100%'} size={5}>
-                <Paper
-                    sx={{ justifyContent: 'center', display: 'flex', flex: 1, height: '100%' }}
-                    variant={'outlined'}
-                >
-                    <Stack alignItems={'center'} height={'100%'} py={2} spacing={2} width={'90%'}>
+        <Stack
+            direction={mobile ? 'column' : 'row'}
+            height={mobile ? undefined : '100vh'}
+            p={2}
+            spacing={2}
+        >
+            <Stack
+                border={'1px solid red'}
+                flex={mobile ? undefined : 5}
+                height={mobile ? 500 : undefined}
+            >
+                <CustomPaper sx={{ height: '100%', px: 0 }}>
+                    <Stack
+                        alignItems={'center'}
+                        height={'100%'}
+                        spacing={2}
+                        width={mobile ? '100%' : '90%'}
+                    >
                         <FormationSelector
                             changeFormation={changeFormation}
                             formationSelected={formation}
                         />
-                        <Box
-                            sx={{
-                                position: 'relative',
-                                width: '100%',
-                                height: '100%',
-                                maxWidth: 800,
-                                maxHeight: 800,
-                                backgroundImage: `url(${pitch})`,
-                                backgroundSize: 'contain',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                            }}
-                        >
-                            <Team />
-                        </Box>
+                        <BoxTeam />
                         <Market />
                         <FormationAdjustment />
                     </Stack>
-                </Paper>
-            </Grid2>
-            <Grid2 size={7}>
-                <Stack height={'100%'} spacing={2}>
-                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
-                        <Typography>{'90% de passes certos'}</Typography>
-                    </Paper>
-                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
-                        <Typography>{'10 desarmes por jogo'}</Typography>
-                    </Paper>
-                    <Paper sx={{ padding: 2 }} variant={'outlined'}>
-                        <Typography>{'5 gols por jogo'}</Typography>
-                    </Paper>
+                </CustomPaper>
+            </Stack>
+            <Stack>
+                <Stack spacing={2}>
+                    <StatCard />
+                    <StatCard />
                 </Stack>
-            </Grid2>
-        </Grid2>
+            </Stack>
+            <Stack>
+                <Stack spacing={2}>
+                    <StatCard />
+                    <StatCard />
+                </Stack>
+            </Stack>
+            <Stack>
+                <Stack spacing={2}>
+                    <StatCard />
+                    <StatCard />
+                </Stack>
+            </Stack>
+            <Stack>
+                <Stack spacing={2}>
+                    <StatCard />
+                    <StatCard />
+                </Stack>
+            </Stack>
+            <Stack>
+                <Stack spacing={2}>
+                    <StatCard />
+                    <StatCard />
+                </Stack>
+            </Stack>
+        </Stack>
     )
 }
 
