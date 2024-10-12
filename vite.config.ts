cdbf0e96b0c669f8bd6266f68c8683fa2c09ba77
casalecchi <felipevilela191@gmail.com>
2024-10-12 10:56:37 -0300

    change vite for dev mode only and vercel to index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
     server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3004',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '/api'),
-            },
-        },
+        proxy:
+            mode === 'development'
+                ? {
+                      '/api': {
+                          target: 'http://localhost:3004',
+                          changeOrigin: true,
+                          rewrite: (path) => path.replace(/^\/api/, '/api'),
+                      },
+                  }
+                : undefined,
     },
-})
+    build: {
+        outDir: 'dist',
+    },
+}))
