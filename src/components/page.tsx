c8cb23256311d0276513a7d650c911b38701cf76
casalecchi <felipevilela191@gmail.com>
2024-10-13 12:37:31 -0300

    add settings on page

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react'
 import { Stack } from '@mui/material'
 import Cover from './cover'
 import HowItWorks from './howItWorks'
+import SettingsMenu from './settingsMenu'
 
 const Page: FC = () => {
     return (
         <Stack display={'flex'}>
             <Cover />
             <HowItWorks />
+            <SettingsMenu />
         </Stack>
     )
 }
