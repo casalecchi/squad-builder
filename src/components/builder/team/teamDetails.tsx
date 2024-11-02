03ba0f69d2ad2529653018d6648c120bfc2f950f
casalecchi <felipevilela191@gmail.com>
2024-11-02 07:02:21 -0300

    new button for model

--- src/components/builder/team/teamDetails.tsx
+++ src/components/builder/team/teamDetails.tsx
@@ -1,7 +1,9 @@
-import { Stack, Typography } from '@mui/material'
+import { InfoOutlined } from '@mui/icons-material'
+import { Button, Stack, Tooltip, Typography } from '@mui/material'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDataContext } from '../../../contexts/DataContext'
+import colors from '../../../styles/colors.module.scss'
 import { formatNumber } from '../../../utils'
 import { CustomPaper } from '../../ui/customPaper'
 
@@ -9,11 +11,26 @@ export const TeamDetails: FC = () => {
     const { t } = useTranslation()
     const { teamStateManager } = useDataContext()
 
+    const value = formatNumber(teamStateManager.teamValue)
+
     return (
         <CustomPaper sx={{ flex: 0 }}>
-            <Stack alignItems={'center'}>
-                <Typography>{t('builder.teamValue')}</Typography>
-                <Typography>{`${t('common.money')}${formatNumber(teamStateManager.teamValue)}`}</Typography>
+            <Stack direction={'row'} justifyContent={'space-between'} width={'100%'}>
+                <Stack alignItems={'center'}>
+                    <Typography>{t('builder.teamValue').toUpperCase()}</Typography>
+                    <Typography
+                        sx={{
+                            color: value > 120 ? colors.gold : 'white',
+                            textShadow: '0px 2px 2px black',
+                        }}
+                    >{`${t('common.money')}${value}`}</Typography>
+                </Stack>
+                <Button variant="outlined">
+                    <Typography mr={0.5}>{t('common.optimize').toUpperCase()}</Typography>
+                    <Tooltip title={t('model.description')}>
+                        <InfoOutlined sx={{ fontSize: '1.1rem' }} />
+                    </Tooltip>
+                </Button>
             </Stack>
         </CustomPaper>
     )
